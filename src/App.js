381cb00dc289dcd7b495d3ae469080d90a65748b
ajay272191 <ajayrmavs33236@gmail.com>
2021-07-20 08:07:04 +0530

    Props, vanilla React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 //component1
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Luna"),
-    React.createElement("h3", {}, "Dog"),
-    React.createElement("h3", {}, "Havanese"),
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
   ])
 }
-// component2
+// main component
 const App = () =>{
   return React.createElement(
   "div",
   {},
   [
-    React.createElement("h1", {}, "Adopt Me!"),
-    ...[1,2,3,4].map((i) => React.createElement("h2", {}, i)),  //spread operator ...[1,2,3]
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement("h1", {id:"my brand"}, "Adopt Me!"),
+    React.createElement(Pet, {name: "Luna", animal:"Dog", breed: "Havanese"}),
+    React.createElement(Pet, {name: "Pepper", animal:"Bird", breed: "Cockatial"}),
+    React.createElement(Pet, {name: "Sudo", animal:"Dog", breed: "Wheaten Terrier"}),
   ]// [] (array) is technically optional to use for grouping but this is how react will handle internally if we dont use []
   )
 }
